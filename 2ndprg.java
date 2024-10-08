7755b4261ecde0fa58cdc0f1bcd8ffb8af6403e0
Vinayak1205 <143931917+Vinayak1205@users.noreply.github.com>
2024-10-08 13:08:06 +0530

    Create 2ndprg.java

---
+++ 2ndprg.java
@@ -0,0 +1,18 @@
+
+class HelloWorld {
+    
+    void greetings(){
+         System.out.println("Hello");
+    }
+    
+     void GoodNight(){
+         System.out.println("GoodNight");
+    }
+    public static void main(String[] args) {
+        HelloWorld obj = new HelloWorld();
+        
+        obj.greetings();
+        obj.GoodNight();
+        
+    }
+}
