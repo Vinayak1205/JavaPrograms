1d75f6bdf7df2ab6b4b20e3a6ef4ba0b8f4d67e6
Vinayak1205 <143931917+Vinayak1205@users.noreply.github.com>
2024-10-09 07:54:57 +0530

    Update 3rdProgram.java

--- 3rdProgram.java
+++ 3rdProgram.java
@@ -11,7 +11,7 @@ class Student {
     
     
     
-    int CalculateCGPA(int grades[],int credits[],int NoOfCredits){
+    static float CalculateCGPA(Student s,int NoOfCredits){
         
         int sum = 0;
         
@@ -19,37 +19,16 @@ class Student {
             sum += s.credits[i]*s.grades[i];
         }
         
-        return sum/NoOfCredits;
+        return (float)sum/NoOfCredits;
     }
- 
     
-    public static void main(String[] args) {
-        
-        Student s = new Student();
-        
-        int SumOfCredits=0;
-        
-        Scanner sc = new Scanner(System.in); // Corrected to use System.in
-        
-        System.out.println("Enter the USN: ");
-        s.usn = sc.nextInt();
-        sc.nextLine(); // Consume the newline character after nextInt()
-        
-        System.out.println("Enter the Name: ");
-        s.name = sc.nextLine();
-        
-        System.out.println("Enter 5 Subject Credits: ");
-        for (int i = 0; i < s.credits.length; i++) 
-        
-            s.credits[i] = sc.nextInt();
-        
-        System.out.println("Enter 5 Subject Marks: ");
-        for (int i = 0; i < s.marks.length; i++) // Use s.marks.length instead of marks.size()
-            s.marks[i] = sc.nextInt();
-        
-        System.out.println("Student Details: ");
+    public static void displayStudentDetails(Student s){
+           System.out.println("Student Details: ");
         System.out.println("Student USN: " + s.usn);
         System.out.println("Student Name: " + s.name);
+        
+         int SumOfCredits=0;
+        
         
         System.out.println("Credits of 5 Subjects: ");
         for (int i = 0; i < s.credits.length; i++){ // Use s.credits.length instead of credits.size()
@@ -61,8 +40,11 @@ class Student {
         for (int i = 0; i < s.marks.length; i++) // Use s.marks.length instead of marks.size()
             System.out.print(s.marks[i] + " ");
             
-            
-        for(int i=0; i<s.marks.length; i++){
+    }
+    
+    public static void ConvertMarksToGrades(Student s){
+        
+         for(int i=0; i<s.marks.length; i++){
                 
                 if(s.marks[i] == 100)
                     s.grades[i] = 10;
@@ -75,8 +57,41 @@ class Student {
         System.out.println("Grades of 5 Subjects: ");
         for (int i = 0; i < s.grades.length; i++) // Use s.marks.length instead of marks.size()
             System.out.print(s.grades[i] + " ");
+    }
+ 
+    
+    public static void main(String[] args) {
+        
+        Student s = new Student();
+        int SumOfCredits = 0;
+         
+       
+        Scanner sc = new Scanner(System.in); // Corrected to use System.in
+        
+        System.out.println("Enter the USN: ");
+        s.usn = sc.nextInt();
+        sc.nextLine(); // Consume the newline character after nextInt()
+        
+        System.out.println("Enter the Name: ");
+        s.name = sc.nextLine();
+        
+        System.out.println("Enter 5 Subject Credits: ");
+        for (int i = 0; i < s.credits.length; i++) {
+           
+            s.credits[i] = sc.nextInt();
+            SumOfCredits += s.credits[i];
+        }
+        System.out.println("Enter 5 Subject Marks: ");
+        for (int i = 0; i < s.marks.length; i++) // Use s.marks.length instead of marks.size()
+            s.marks[i] = sc.nextInt();
+        
+     
+        displayStudentDetails(s);
             
-        int res = CalculateCGPA(grades,credits,SumOfCredits);
+        ConvertMarksToGrades(s);
+       
+            
+        float res = CalculateCGPA(s,SumOfCredits);
          System.out.print("Your CGPA:  "+res);
         sc.close(); // Close the scanner
     }
