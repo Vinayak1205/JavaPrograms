7755660a3dab67ca448af279319a0212e199b40f
Vinayak1205 <143931917+Vinayak1205@users.noreply.github.com>
2024-11-12 12:14:42 +0530

    Create Check Whether a string starts with another.java

---
+++ Check Whether a string starts with another.java
@@ -0,0 +1,16 @@
+class Main1{
+
+	public static void main(String args[]){
+
+		String str1 = "Hello, World!";
+		String str2 = "How!";
+        
+        
+        	if(str1.startsWith(str2))
+			System.out.println("true");
+		else
+		System.out.println("false");
+
+			
+	}
+}
