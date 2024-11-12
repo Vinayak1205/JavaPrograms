1488897fd118127f543758987ea4ff1c5db02494
Vinayak1205 <143931917+Vinayak1205@users.noreply.github.com>
2024-11-12 12:14:13 +0530

    Create String converting to characters.java

---
+++ String converting to characters.java
@@ -0,0 +1,15 @@
+class Main{
+
+	public static void main(String args[]){
+
+		String str = "Hello, World!";
+        
+        
+        	char[] charArray = str.toCharArray();
+        
+     
+        	for (char c : charArray) {
+            		System.out.print(c + " ");
+        	}
+	}
+}
