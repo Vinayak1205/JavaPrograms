5dd7977eadf95555680ad0ae9837de9322baf235
Vinayak1205 <143931917+Vinayak1205@users.noreply.github.com>
2024-11-12 12:16:56 +0530

    Create ConcatenateTwoStrings.java

---
+++ ConcatenateTwoStrings.java
@@ -0,0 +1,14 @@
+class Main2{
+
+	public static void main(String args[]){
+
+		String str1 = "Hello, World!";
+		String str2 = "How!";
+        
+        
+        	String str3 = str1+str2;
+		System.out.println("After concatenating two string the resultant string is: "+str3);
+		
+			
+	}
+}
